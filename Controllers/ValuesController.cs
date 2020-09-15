a2c4a4ac687b39e7b4551ce8a9089bbe488f5006
Roman Onofreichuk <onofreichukroman@gmail.com>
2020-09-15 13:41:46 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using MedicalTelegrammBot.Models;
+
+namespace MedicalTelegrammBot.Controllers
+{
+    [Route("api/message/update")]
+    public class MessageController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Method GET unavailable";
+        }
+    }
+}
