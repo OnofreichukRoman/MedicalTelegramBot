35972d197870ba21a203f6118ee6db4e3cf218e6
Roman Onofreichuk <onofreichukroman@gmail.com>
2020-09-15 15:59:55 +0300

    Created "StartCommand" class.

---
+++ Models/Commands/StartCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MedicalTelegrammBot.Models.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => "/start";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                return false;
+            }
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId, "Привет, если ты это видишь значит Рома таки все сделал.",parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
