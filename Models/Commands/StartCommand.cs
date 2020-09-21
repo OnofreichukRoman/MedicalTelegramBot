eb19682be99dce5a78ff51479a88b9424421ad7f
Roman Onofreichuk <onofreichukroman@gmail.com>
2020-09-22 01:24:22 +0300

    Added a start keyboard

--- Models/Commands/StartCommand.cs
+++ Models/Commands/StartCommand.cs
@@ -20,10 +20,29 @@ namespace MedicalTelegrammBot.Models.Commands
 
         public override async Task Execute(Message message, TelegramBotClient botClient)
         {
-            var chatId = message.Chat.Id;
-            string botTextAnswer = $"[This is picture](https://thepresentation.ru/img/thumbs/dc3e75780cc1225ce8dd917e1f047e49-800x.jpg) *Привет*, если ты это видишь значит Рома таки все сделал. Твой chatId: {chatId}. Можешь использовать новую команду /keyboardTest ";
+            var keyboard = new Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup
+            {
+                Keyboard = new[]
+                {
+                     new[] // row 1
+                     {
+                          new Telegram.Bot.Types.ReplyMarkups.KeyboardButton("Анализы"),
+                     },
+                     new[]
+                     {
+                          new Telegram.Bot.Types.ReplyMarkups.KeyboardButton("Первая помощь")
+                     }
+                },
+                ResizeKeyboard = true
+            };
 
-            await botClient.SendTextMessageAsync(chatId, botTextAnswer, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            var chatId = message.Chat.Id;
+            var userName = message.From.Username;
+            string botTextAnswer = $"Здравствуйте, *{userName}*!";
+            string botKeyboardMessage = "Сделайте свой выбор:";
+
+            await botClient.SendPhotoAsync(chatId, "https://intalent.pro/sites/default/files/styles/new_photo_in_article/public/foto/article/ai-and-meds.jpg?itok=3ryePX_S", botTextAnswer, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            await botClient.SendTextMessageAsync(message.Chat.Id, botKeyboardMessage, replyMarkup: keyboard);
         }
     }
 }
