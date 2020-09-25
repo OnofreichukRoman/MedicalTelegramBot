bda831aebb385959df5d5654d8feb3192ada7944
Roman Onofreichuk <onofreichukroman@gmail.com>
2020-09-25 14:59:39 +0300

    Added Command summary

--- Models/Commands/Command.cs
+++ Models/Commands/Command.cs
@@ -4,10 +4,23 @@ using Telegram.Bot.Types;
 
 namespace MedicalTelegrammBot.Models.Commands
 {
+    /// <summary>
+    /// Defines the abstract bot command <see cref="Command" />.
+    /// </summary>
     internal abstract class Command
     {
+        /// <summary>
+        /// Gets the command name.
+        /// </summary>
         internal abstract string Name { get; }
 
+        /// <summary>
+        /// Execute the command.
+        /// </summary>
+        /// <param name="message">Incoming message <see cref="Update.Message"/>.</param>
+        /// <param name="client">The bot client <see cref="TelegramBotClient"/>.</param>
+        /// <returns>Command execution <see cref="Task"/>.</returns>
+        ///
         internal abstract Task Execute(Message message, TelegramBotClient client);
     }
 }
